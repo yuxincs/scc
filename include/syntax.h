bb62c16c0b10d454b81aafcba749f66a0a59ab7e
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-09-07 00:33:50 +0800

    Add correct Syntax structure.

--- include/syntax.h
+++ include/syntax.h
@@ -1,17 +1,156 @@
 #ifndef SCC_SYNTAX_HEADER
 #define SCC_SYNTAX_HEADER
 
+/* Typedefs for syntax types  */
+typedef enum 
+{
+    IMMEDIATE,
+    VARIABLE,
+    UNARY_OPERATOR,
+    BINARY_OPERATOR,
+    BLOCK,
+    IF_STATEMENT,
+    RETURN_STATEMENT,
+    DEFINE_VAR,
+    FUNCTION,
+    FUNCTION_CALL,
+    FUNCTION_ARGUMENTS,
+    ASSIGNMENT,
+    WHILE_SYNTAX,
+    TOP_LEVEL
+} SyntaxType;
+
+typedef enum
+{
+    INT,
+    FLOAT,
+    DOUBLE,
+    ARRAY,
+    STRUCT
+} VariableType;
+
+typedef enum 
+{ 
+    BITWISE_NEGATION,
+    LOGICAL_NEGATION
+} UnaryExpressionType;
+
+typedef enum 
+{
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    LESS_THAN,
+    LESS_THAN_OR_EQUAL,
+} BinaryExpressionType;
+
+typedef struct Immediate 
+{ 
+    int value; 
+} Immediate;
+
+typedef struct Variable 
+{
+    VariableType type;
+    char *name;
+} Variable;
+
+typedef struct UnaryExpression 
+{
+    UnaryExpressionType type;
+    Syntax *expression;
+} UnaryExpression;
+
+typedef struct BinaryExpression 
+{
+    BinaryExpressionType type;
+    Syntax *left;
+    Syntax *right;
+} BinaryExpression;
+
+typedef struct FunctionArguments 
+{ 
+    List *arguments; 
+} FunctionArguments;
+
+typedef struct FunctionCall 
+{
+    char *name;
+    Syntax *arguments;
+} FunctionCall;
+
+typedef struct Assignment 
+{
+    char *name;
+    Syntax *expression;
+} Assignment;
+
+typedef struct IfStatement 
+{
+    Syntax *condition;
+    Syntax *then;
+} IfStatement;
+
+typedef struct DefineVarStatement 
+{
+    char *name;
+    Syntax *init_value;
+} DefineVarStatement;
+
+typedef struct WhileStatement 
+{
+    Syntax *condition;
+    Syntax *body;
+} WhileStatement;
+
+typedef struct ReturnStatement 
+{ 
+    Syntax *expression; 
+} ReturnStatement;
+
+typedef struct Block 
+{ 
+    List *statements; 
+} Block;
+
+typedef struct Function {
+    char *name;
+    List *parameters;
+    Syntax *root_block;
+} Function;
+
+typedef struct Parameter 
+{
+    // TODO: once we have other types, we will need to store type here.
+    char *name;
+} Parameter;
+
+typedef struct TopLevel 
+{ 
+    List *declarations; 
+} TopLevel;
+
 typedef struct _Syntax
 {
-    int type;
-    union value_union{
-        int int_value;
-        char string_value[128];
-    }value;
-    int lineno;
-    struct _Syntax * upper_layer;
-    struct _Syntax * next;
-    struct _Syntax * lower_layer;
+    SyntaxType type;
+    union {
+        Immediate *immediate;
+        Variable *variable;
+        UnaryExpression *unary_expression;
+        BinaryExpression *binary_expression;
+        Assignment *assignment;
+        ReturnStatement *return_statement;
+        FunctionArguments *function_arguments;
+        FunctionCall *function_call;
+        IfStatement *if_statement;
+        DefineVarStatement *define_var_statement;
+        WhileStatement *while_statement;
+        Block *block;
+        Function *function;
+        TopLevel *top_level;
+    };
 }Syntax;
+
+
 
 #endif
