b3a5a0d480441f285c7e4889db0f552643ebe314
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-09-06 23:58:42 +0800

    Alter the function name.

--- include/list.h
+++ include/list.h
@@ -1,7 +1,8 @@
 #ifndef SCC_LIST_HEADER
 #define SCC_LIST_HEADER
 
-typedef struct List {
+typedef struct List 
+{
     int size;
     void **items;
 } List;
@@ -9,19 +10,12 @@ typedef struct List {
 #define INITIAL_LIST_SIZE 32
 
 List *list_new(void);
-
 int list_length(List *list);
-
 void list_free(List *list);
-
 void list_append(List *list, void *item);
-
 void list_push(List *list, void *item);
-
 void *list_pop(List *list);
-
 void *list_get(List *list, int index);
-
 void list_set(List *list, int index, void *value);
 
 #endif
