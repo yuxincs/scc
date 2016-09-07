4e1f2c82ac41e4692b148e60261684ea017f013f
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-09-07 10:57:42 +0800

    Clean yacc file, move other functions out of it.

--- source/main.c
+++ source/main.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
-#include "y.tab.h"
+#include "list.h"
+#include "syntax.h"
+#include "scc_yacc.hpp"
+
+extern FILE * yyin;
+extern char *file_content[1024];
+extern char file_name[1024];
+extern int yyparse();
+
+void read_file()
+{
+    for(int i = 0; !feof(yyin); i++)
+    {
+        file_content[i] = (char * )malloc(1024 * sizeof(char));
+        fgets(file_content[i], 1024, yyin);
+        file_content[i][strlen(file_content[i]) - 1] = '\0';
+    }
+    fseek(yyin, 0, 0);
+}
 
 int main(int argc, char ** argv)
 {
+    // read from file
     if(argc > 1)
     {
+        strcpy(file_name, argv[1]);
         yyin = fopen(argv[1], "r");
         if(yyin == NULL)
         {
             printf("File doesn't exits.\n");
             return 1;
         }
-        strcpy(file_name, argv[1]);
-        
-        if(argc > 2)
-        {
-            if(strcmp(argv[2], "-print-tree") == 0)
-                is_to_print = true;
-            
-            
-        }
-    }    
+    }
+    // TODO: macro expansion
+
+    // grammer analysis
     read_file();
-    yyparse();
-    if(!has_error)
-        printf("\033[1;32mCompile Success!\033[0m\n");
+    if(yyparse() != 0)
+        return 0;
+
+    // semantic analysis
+    
+    printf("\033[1;32mCompile Success!\033[0m\n");
     return 0;
 }
 
