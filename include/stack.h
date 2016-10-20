078fec9d13197125178e99e729bb0fb5fcdb44ff
RyanWangGit <wangyuxin@hust.edu.cn>
2016-10-20 13:19:50 +0800

    Use `new` and `delete`.

--- include/stack.h
+++ include/stack.h
@@ -8,8 +8,8 @@ typedef struct _Stack {
     void **content;
 } Stack;
 
-Stack *stack_create();
-void stack_free(Stack *stack);
+Stack *stack_new();
+void stack_delete(Stack *stack);
 void stack_push(Stack *stack, void *item);
 void *stack_pop(Stack *stack);
 void *stack_peek(Stack *stack);
--- source/list.c
+++ source/list.c
@@ -4,7 +4,7 @@
 #include <err.h>
 #include "list.h"
 
-List *list_create(void)
+List *list_new(void)
 {
     List *list = malloc(sizeof(List));
     list->size = 0;
@@ -13,7 +13,7 @@ List *list_create(void)
     return list;
 };
 
-void list_free(List *list) 
+void list_delete(List *list) 
 {
     if (list->items != NULL) {
         free(list->items);
