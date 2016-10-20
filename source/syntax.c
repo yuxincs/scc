e0e42f621fb301068d67abf41c74aa47f7f54bd9
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-10-20 13:26:29 +0800

    Complete syntax_new function.

--- source/syntax.c
+++ source/syntax.c
@@ -1,4 +1,136 @@
+#include <stdlib.h>
 #include "syntax.h"
+
+char * string_new(int size)
+{
+    char * buf = (char *)malloc(sizeof(char) * size);
+    buf[0] = '\0';
+    return buf;
+}
+
+Syntax * default_syntax_new(SyntaxType type)
+{
+    Syntax * syntax = (Syntax *)malloc(sizeof(Syntax));
+    syntax->type = type;
+    switch(type)
+    {
+        case IMMEDIATE:
+        {
+            syntax->immediate = (Immediate *)malloc(sizeof(Immediate));
+            syntax->immediate->value = 0;
+            break;
+        }
+        case VARIABLE:
+        {
+            syntax->variable = (Variable *)malloc(sizeof(Variable));
+            syntax->variable->type = INT;
+            syntax->variable->name = (char *)malloc(sizeof(char) * 33);
+            break;
+        }
+        case UNARY_EXPRESSION:
+        {
+            syntax->unary_expression = (UnaryExpression *)malloc(sizeof(UnaryExpression));
+            syntax->unary_expression->type = BITWISE_NEGATION;
+            syntax->unary_expression->expression = NULL;
+            break;
+        }
+        case BINARY_EXPRESSION:
+        {
+            syntax->binary_expression = (BinaryExpression *)malloc(sizeof(BinaryExpression));
+            syntax->binary_expression->type = ADDITION;
+            syntax->binary_expression->left = NULL;
+            syntax->binary_expression->right = NULL;
+            break;
+        }
+        case BLOCK:
+        {
+            syntax->block = (Block *)malloc(sizeof(Block));
+            syntax->block->statements = NULL;
+            break;
+        }
+        case IF_STATEMENT:
+        {
+            syntax->if_statement = (IfStatement *)malloc(sizeof(IfStatement));
+            syntax->if_statement->condition = NULL;
+            syntax->if_statement->then = NULL;
+            break;
+        }
+        case RETURN_STATEMENT:
+        {
+            syntax->return_statement = (ReturnStatement *)malloc(sizeof(ReturnStatement));
+            syntax->return_statement->expression = NULL;
+            break;
+        }
+        case DEFINE_VARIABLE_STATEMENT:
+        {
+            syntax->define_var_statement = (DefineVarStatement *)malloc(sizeof(DefineVarStatement));
+            syntax->define_var_statement->name = string_new(33);
+            syntax->define_var_statement->init_value = NULL;
+            break;
+        }
+        case FUNCTION:
+        {
+            syntax->function = (Function *)malloc(sizeof(Function));
+            syntax->function->name = string_new(33);
+            syntax->function->parameters = NULL;
+            syntax->function->root_block = NULL;
+            break;
+        }
+        case FUNCTION_CALL:
+        {
+            syntax->function_call = (FunctionCall *)malloc(sizeof(FunctionCall));
+            syntax->function_call->name = string_new(33);
+            syntax->function_call->arguments = NULL;
+            break;
+        }
+        case FUNCTION_ARGUMENTS:
+        {
+            syntax->function_arguments = (FunctionArguments *)malloc(sizeof(FunctionArguments));
+            syntax->function_arguments->arguments = NULL;
+            break;
+        }
+        case ASSIGNMENT:
+        {
+            syntax->assignment = (Assignment *)malloc(sizeof(Assignment));
+            syntax->assignment->name = string_new(33);
+            syntax->assignment->expression = NULL;
+            break;
+        }
+        case WHILE_SYNTAX:
+        {
+            syntax->while_statement = (WhileStatement *)malloc(sizeof(WhileStatement));
+            syntax->while_statement->condition = NULL;
+            syntax->while_statement->body = NULL;
+            break;
+        }
+        case TOP_LEVEL:
+        {
+            syntax->top_level = (TopLevel *)malloc(sizeof(TopLevel));
+            syntax->top_level->definitions = NULL;
+            break;
+        }
+        default: break;
+    }
+    return syntax;
+}
+
+void default_syntax_delete(Syntax * syntax)
+{
+
+}
+
+Syntax * syntax_new(SyntaxType type)
+{
+    return default_syntax_new(type);
+}
+
+void syntax_delete(Syntax * syntax)
+{
+    default_syntax_delete(syntax);
+    return;
+}
+
+
 
 /*
 Syntax * create_string_node(int type, char string_value[128])
