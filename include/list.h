d6f1db050feb3c7b6b818c14c0e47bf88f03ea75
Ryan Wang <wangyuxin@hust.edu.cn>
2016-10-23 22:55:38 +0800

    Add a new function.

--- include/list.h
+++ include/list.h
@@ -14,6 +14,7 @@ void list_delete(List *list);
 int list_length(List *list);
 void list_append(List *list, void *item);
 void list_prepend(List *list, void *item);
+void list_append_list(List *dest, List *source);
 void list_push(List *list, void *item);
 void *list_pop(List *list);
 void *list_get(List *list, int index);
