4932d17bf092ee88caef76f5f4ad609034b0a9df
RyanWangGit <wangyuxin@hust.edu.cn>
2016-10-31 20:02:14 +0800

    Add new empty semantic analysis file.

---
+++ include/semantic.h
@@ -0,0 +1,8 @@
+#ifndef SCC_SEMANTIC_HEADER
+#define SCC_SEMANTIC_HEADER
+
+#include "syntax.h"
+
+void semantic_analysis(Syntax * top_level);
+
+#endif
---
+++ source/semantic.c
@@ -0,0 +1,6 @@
+#include "semantic.h"
+
+void semantic_analysis(Syntax * top_level)
+{
+    return;
+}
