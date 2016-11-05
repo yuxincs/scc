7b381872edda6ad07b10c966515cfef7d12806c0
RyanWangGit <wangyuxin@hust.edu.cn>
2016-11-05 11:27:13 +0800

    Simplify Symbol structure.

--- source/symbol.c
+++ source/symbol.c
@@ -1,34 +1,14 @@
 #include <stdlib.h>
+#include "commonutils.h"
 #include "symbol.h"
 
-Symbol * symbol_new(SymbolType type)
+
+Symbol * symbol_new()
 {
     Symbol * symbol = (Symbol *)malloc(sizeof(Symbol));
-    symbol->type = type;
-    switch(symbol->type)
-    {
-        case VARIABLESYMBOK:
-        {
-            symbol->variable_symbol = (VariableSymbol *)malloc(sizeof(VariableSymbol));
-
-            break;
-        }
-        case FUNCTIONSYMBOL:
-        {
-            symbol->function_symbol = (FunctionSymbol *)malloc(sizeof(FunctionSymbol));
-            break;
-        }
-        case STRUCTSYMBOL:
-        {
-            symbol->struct_symbol = (StructSymbol *)malloc(sizeof(StructSymbol));
-            break;
-        }
-        default: 
-        {
-            symbol->variable_symbol = NULL;
-            break;
-        }
-    }
+    symbol->level = -1;
+    symbol->name = string_new(33);
+    symbol->declaration = NULL;
     return symbol;
 }
 
