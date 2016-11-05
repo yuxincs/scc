648ce1a57399e5d766732688d4abe8c4c1d7316d
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-11-05 11:27:13 +0800

    Simplify Symbol structure.

--- include/symbol.h
+++ include/symbol.h
@@ -1,40 +1,16 @@
 #ifndef SCC_SYMBOL_HEADER
 #define SCC_SYMBOL_HEADER
 
-typedef enum
-{
-    VARIABLESYMBOL,
-    FUNCTIONSYMBOL,
-    STRUCTSYMBOL
-} SymbolType;
-
-typedef struct VariableSymbol
-{
-
-} VariableSymbol;
-
-typedef struct FunctionSymbol
-{
-
-} FunctionSymbol;
-
-typedef struct StructSymbol
-{
-
-} StructSymbol;
+#include "syntax.h"
 
 typedef struct Symbol
 {
-    SymbolType type;
-    union
-    {
-        VariableSymbol * variable_symbol;
-        FunctionSymbol * function_symbol;
-        StructSymbol * struct_symbol;
-    };
+    int level;
+    char *name;
+    Syntax * declaration;
 } Symbol;
 
-Symbol * symbol_new(SymbolType type);
+Symbol * symbol_new();
 
 void symbol_delete(Symbol * symbol);
 
