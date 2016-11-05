093643bd8c0ac1b3d79cc6e7a4c125a9c8fef4e9
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-11-05 11:27:56 +0800

    Use List as basic implementation of symboltable.

--- include/symboltable.h
+++ include/symboltable.h
@@ -1,14 +1,21 @@
 #ifndef SCC_SYMBOLTABLE_HEADER
 #define SCC_SYMBOLTABLE_HEADER
+#include <stdlib.h>
+#include <string.h>
 #include "symbol.h"
+#include "list.h"
 
 typedef struct _SymbolTable
 {
-
+    List *list;
 } SymbolTable;
 
-Symbol * get_symbol(SymbolTable * table);
+SymbolTable * new_symbol_table();
+
+Symbol * get_symbol(SymbolTable * table, char * name);
 
 void insert_symbol(SymbolTable * table, Symbol * symbol);
+
+void remove_level(SymbolTable * tabel, int level);
 
 #endif
