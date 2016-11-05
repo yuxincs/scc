fc758c82ae3c4920a6caa8b5997343135a551789
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-11-05 11:29:18 +0800

    Add undefined variable and duplicate declaration.

--- tests/full_test.c
+++ tests/full_test.c
@@ -1,5 +1,5 @@
 
-int a;
+int global_a;
 struct foo
 {
     int a;
@@ -9,9 +9,13 @@ struct foo
 int func(int c, int d, float f)
 {
     int b = 1;
+    float test_array[10];
     b = c;
     b = c * d + 6 * 7.0;
     h = !f;
+    b[1] = 2;
+    nono[0] = 3;
+    
 
     return c * d;
 }
@@ -19,9 +23,11 @@ int func(int c, int d, float f)
 void main()
 {
     int aaa[10];
+    int duplicate[1];
+    int duplicate;
     struct foo bar;
     bar.a = 1;
-    func(2.5, a, a[0]);
+    func(2.5, a, aaa[0]);
     return;
 }
 
