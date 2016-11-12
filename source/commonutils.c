379ea253e1c4ba0862d8a7b4250352b198d49425
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-11-13 00:31:34 +0800

    Check if error_part is null.

--- source/commonutils.c
+++ source/commonutils.c
@@ -17,18 +17,21 @@ void print_error(char *msg, char *error_part, int lineno)
 { 
     printf("\033[1m%s [%d] \033[1;31merror:\033[0m\033[1m %s \033[0m\n", file_name, lineno, msg);
     printf("%s\n", file_content[lineno - 1]);
-    int index = -1;
-    char * pos = strstr(file_content[lineno - 1], error_part);
-    if(pos != NULL)
-        index = pos - file_content[lineno - 1];
-    
-    printf("\033[1;32m");
-    for(int i = 0;i < index; ++i)
-        putchar(' ');
-    for(int j = 0;j < strlen(error_part);j ++)
-        putchar('~');
-    putchar('\n');
-    printf("\033[0m");
+    if(error_part != NULL)
+    {
+        int index = -1;
+        char * pos = strstr(file_content[lineno - 1], error_part);
+        if(pos != NULL)
+            index = pos - file_content[lineno - 1];
+        
+        printf("\033[1;32m");
+        for(int i = 0;i < index; ++i)
+            putchar(' ');
+        for(int j = 0;j < strlen(error_part);j ++)
+            putchar('~');
+        putchar('\n');
+        printf("\033[0m");
+    }
     return;
 }
 
@@ -36,17 +39,21 @@ void print_note(char *msg, char *note_part, int lineno)
 {
     printf("\033[1m%s [%d] note: %s \033[0m\n", file_name, lineno, msg);
     printf("%s\n", file_content[lineno - 1]);
-    int index = -1; 
-    char * pos = strstr(file_content[lineno - 1], note_part);
-    if(pos != NULL)
-        index = pos - file_content[lineno - 1];
+    if(note_part != NULL)
+    {
+        int index = -1; 
+        char * pos = strstr(file_content[lineno - 1], note_part);
+        if(pos != NULL)
+            index = pos - file_content[lineno - 1];
+        
+        printf("\033[1;32m");
+        for(int i = 0;i < index;i ++)
+            putchar(' ');
+        for(int j = 0;j < strlen(note_part);j ++)
+            putchar('~');
+        putchar('\n');
+        printf("\033[0m");
+    }
     
-    printf("\033[1;32m");
-    for(int i = 0;i < index;i ++)
-        putchar(' ');
-    for(int j = 0;j < strlen(note_part);j ++)
-        putchar('~');
-    putchar('\n');
-    printf("\033[0m");
     return;
 }
