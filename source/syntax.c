ef5e1f6aea94d0449375e39b7af4562b561a69f3
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-11-24 20:13:44 +0800

    Print Block syntax.

--- source/syntax.c
+++ source/syntax.c
@@ -399,8 +399,10 @@ void print_syntax_depth(FILE *fp, Syntax * syntax, int depth)
         }
         case BLOCK:
         {
+            PRINT_SPACE(fp, depth)
+            fprintf(fp, "Block ->\n");
             for(int i = 0; i < list_length(syntax->block->statements); ++i)
-                print_syntax_depth(fp, (Syntax *)list_get(syntax->block->statements, i), depth);
+                print_syntax_depth(fp, (Syntax *)list_get(syntax->block->statements, i), depth + 2);
             break;
         }
         case TOP_LEVEL:
