af566f87df496666907736f04e64db6a10f289db
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-02 16:03:18 -0400

    Only print the last number.

--- tests/fibonacci.c
+++ tests/fibonacci.c
@@ -1,15 +1,15 @@
 int main()
 {
-    int a = 0, b = 1, i = 0, n;
+    int a = 0, b = 1, i = 1, n;
     n = __READINT();
     while(i < n)
     {
         int c = a + b;
-        __WRITEINT(b);
         a = b;
         b = c;
         i = i + 1;
     }
+    __WRITEINT(b);
     return 0;
 }
 
